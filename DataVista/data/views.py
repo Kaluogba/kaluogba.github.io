b23e887111ac07825dbf2a307b4ce113cf68ffa3
kaluogba <kaluogba1990@gmail.com>
2024-04-11 08:01:13 -0700

    My Project commits

--- DataVista/data/views.py
+++ DataVista/data/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -6,6 +9,24 @@ def index(request):
     return render(request, 'data/index.html')
 
 def login_page(request):
+    if request.method == 'POST':
+        # Retrieve username and password from the form
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # User authentication successful, log in the user
+            login(request, user)
+            # Redirect to a different page after login (e.g., dashboard)
+            return redirect('dashboard_page')
+        else:
+            # Authentication failed, render the login page with an error message
+            return render(request, 'data/login.html', {'error_message': 'Invalid username or password'})
+
+    # If the request method is GET, render the login page
     return render(request, 'data/login.html')
 
 def logout_user(request):
@@ -13,7 +34,32 @@ def logout_user(request):
     return render(request, 'data/logout.html')
 
 def register_page(request):
-    return render(request, 'data/register.html')
+    if request.method == 'POST':
+        # Extract form data
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        # Validate and process the data
+        if password != confirm_password:
+            return render(request, 'data/register.html', {'error_message': 'Passwords do not match'})
+
+        # Check if a user with the same username or email already exists
+        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+            return render(request, 'data/register.html', {'error_message': 'Username or email already exists'})
+
+        # Create a new user
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+
+        # Create a corresponding UserProfile instance
+        UserProfile.objects.create(user=new_user)
+
+        # Redirect to login page or any other page
+        return redirect('login_page')
+    else:
+        # Render the registration form
+        return render(request, 'data/register.html')
 
 def profile_page(request):
     # Logic to retrieve and display user profile information
